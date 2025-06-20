b17834b8a35d756e2dea458f3582b6e172261f06
ImaTaku <48483921+ta93imtk@users.noreply.github.com>
2025-06-20 16:50:47 +0900

    fix: eslintのerror

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+
 import { FlatCompat } from '@eslint/eslintrc'
-import importPlugin from 'eslint-plugin-import'
 import js from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
